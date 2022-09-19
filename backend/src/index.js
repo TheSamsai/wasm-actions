d6d816ed0cb356013cb05bca1550963770b6c46e
Sami Lahtinen <samsai@posteo.net>
2022-09-19 19:26:11 +0300

    Basic JWT middleware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const wasi_runner = require('./wasi-runner');
 
+const db = require('./db')
+
 const app = express();
 const port = 3001;
 
@@ -22,10 +24,50 @@ const wasiRunnerMiddleware = function (req, res, next) {
     next();
 }
 
+const verifiedUser = function (req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    const user = db.verify_token(token);
+
+    req.user = user;
+
+    next();
+}
+
 app.use(wasiRunnerMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Hello, universe!");
+})
+
+
+app.get('/hidden', verifiedUser, (req, res) => {
+    if (!req.user) {
+        res.json({
+            "error": "invalid token"
+        });
+    }
+
+
+    res.json({
+        "message": "42"
+    });
+})
+
+app.post('/register', (req, res) => {
+    db.register_user(req.body.username, req.body.password);
+
+    res.json({
+        "message": `Registered user ${req.body.username}`
+    });
+})
+
+app.post('/login', (req, res) => {
+    const token = db.login_user(req.body.username, req.body.password);
+
+    res.json({
+        "token": token
+    });
 })
 
 app.listen(port, () => {
