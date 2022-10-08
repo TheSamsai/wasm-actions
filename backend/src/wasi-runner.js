dedee68ec0e9b8dd1928790a94888e55f24afdd5
Sami Lahtinen <samsai@posteo.net>
2022-10-08 16:02:55 +0300

    Run WASI Runner processes with WasmEdge

--- backend/src/wasi-runner.js
+++ backend/src/wasi-runner.js
@@ -2,14 +2,11 @@ const { spawnSync } = require("child_process");
 
 const execSync = require("child_process").spawnSync;
     
-// const result = execSync("python celulas.py");
-    
-// // convert and show the output.
-// console.log(result.toString("utf8"));
+const PATH_PREFIX = "wasm"
 
 const run_wasi = (file, args) => {
     const result = spawnSync(
-        "node", ["--no-warnings", "--experimental-wasi-unstable-preview1", "./src/wasi-process.mjs", file].concat(args.map(v => v.toString())),
+        "wasmedge", [`${PATH_PREFIX}/${file}`].concat(args.map(v => v.toString())),
         {
             encoding: "utf8"
         }
